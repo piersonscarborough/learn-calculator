34f832edab357cc5cc538ffd1b10407148ea5f21
Pierson Scarborough <piersonscarborough@Piersons-MacBook-Pro.local>
2019-05-09 13:12:39 -0400

    Added calculations functionality

--- js/script.js
+++ js/script.js
@@ -57,8 +57,42 @@ operators.forEach(function(op){
 });
 // on click of 'equal' button
 result.addEventListener('click', function(){
-    input.innerHTML += 'no math';
+    const currentString = input.innerHTML;input.innerHTML;
+    const numberStringArray = currentString.split(/\+|\-|\*|\//g);
+    let numbersArray = [];
+    numberStringArray.forEach(function(number){
+        numbersArray.push(Number(number));
+    });
+
+    const operatorsArray = currentString.replace(/[0-9]|\./g, "").split("");
+
+    let multiply = operatorsArray.indexOf("*");
+    while(multiply != -1){
+        numbersArray.splice(multiply, 2, numbersArray[multiply] * numbersArray[multiply +1])
+        operatorsArray.splice(multiply, 1);
+        multiply = operatorsArray.indexOf('*');
+    }
+    let divide = operatorsArray.indexOf("/");
+    while (divide != -1){
+        numbersArray.splice(divide, 2, numbersArray[divide] / numbersArray[divide + 1])
+        operatorsArray.splice(divide, 1);
+        divide = operatorsArray.indexOf('/');
+    }
+    let add = operatorsArray.indexOf("+");
+    while (add != -1){
+        numbersArray.splice(add, 2, numbersArray[add] + numbersArray[add + 1])
+        operatorsArray.splice(add, 1);
+        add = operatorsArray.indexOf('+');
+    }
+    let subtract = operatorsArray.indexOf("-");
+    while(subtract != -1){
+        numbersArray.splice(subtract, 2, numbersArray[subtract] - numbersArray[subtract + 1])
+        operatorsArray.splice(subtract, 1);
+        subtract = operatorsArray.indexOf('-');
+    }
+    input.innerHTML = numbersArray;
 });
+
 // clearing the input on press of clear
 clear.addEventListener('click', function(){
     input.innerHTML = '';
